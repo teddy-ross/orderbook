d9a06a6c7c5a7115f1dae2f6f34c4ca5578fe0d3
Teddy Ross <tross@mail.wlu.edu>
2025-01-02 16:53:51 -0500

    finished order class

--- orderbook.cpp
+++ orderbook.cpp
@@ -40,7 +40,8 @@ struct LevelInfo{
 
 using LevelInfos = std::vector<LevelInfos>;
 
-class OrderbookLevelInfos{
+//class that contains the information and read methods for our bids and asks
+class OrderbookLevelInfos{ 
 
 public:
     OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
@@ -56,10 +57,46 @@ private:
     LevelInfos asks_;
 };
 
+class Order{
 
+public:
+
+    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_{orderType}
+        , orderId_{orderId}
+        , side_{side}
+        , price_{price}
+        , initialQuantity_{quantity} //need two different types of quantity to describe filled quanitity, initial order and remaining lots
+        , remainingQuantity_{quantity}
+    { }
+
+    OrderId GetOrderId() const { return orderId_; }
+    Side GetSide() const {return side_;}
+    Price GetPrice() const {return price_;}
+    OrderType GetOrderType() const {return orderType_;}
+    Quantity GetInitialQuantity() const {return initialQuantity_;}
+    Quantity GetRemainingQuantity() const {return remainingQuantity_;}
+    Quantity GetFilledQuantity() const {return GetInitialQuantity() - GetRemainingQuantity();}
+
+    void Fill(Quantity quantity){
+
+        if (quantity > GetRemainingQuantity()){
+            throw std::logic_error("Order cannot be filled for more than its remaining quantity.");
+        }
+        remainingQuantity_ -= quantity;
+};
+private:
+
+    OrderType orderType_;
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity initialQuantity_;
+    Quantity remainingQuantity_;
+};
 
 int main(){
 
 
     return 0;
-}
+};
