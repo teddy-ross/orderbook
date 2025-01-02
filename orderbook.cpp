0baa8d1600f2098be41edbf2e42fcffdd9e7594e
Teddy Ross <tross@mail.wlu.edu>
2025-01-02 16:24:53 -0500

    created OrderbookLevelInfos Class

--- orderbook.cpp
+++ orderbook.cpp
@@ -19,6 +19,43 @@
 #include <tuple>
 #include <format>
 
+enum class OrderType{
+    GoodTilCancel,
+    FillAndKill
+};
+
+enum class Side{
+    Buy,
+    Sell
+};
+
+using Price = int32_t;
+using Quantity = uint32_t;
+using OrderId = uint64_t;
+
+struct LevelInfo{
+    Price price_;
+    Quantity quantity_;
+};
+
+using LevelInfos = std::vector<LevelInfos>;
+
+class OrderbookLevelInfos{
+
+public:
+    OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
+        : bids_{ bids }
+        , asks_{ asks }
+    { }
+
+    const LevelInfos& GetBidTrade() const { return bids_; }
+    const LevelInfos& GetAskTrade() const { return asks_; }
+
+private:
+    LevelInfos bids_;
+    LevelInfos asks_;
+};
+
 
 
 int main(){
